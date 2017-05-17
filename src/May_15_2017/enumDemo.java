5bb9357545c0ec039db634128cc3f119ab2d7f07
isaacaj <isaac@fiaike.com>
2017-05-17 08:55:13 -0500

    Enum Demo

---
+++ src/May_15_2017/enumDemo.java
@@ -0,0 +1,19 @@
+package May_15_2017;
+
+/**
+ * Created by 90304755 on 5/15/17.
+ */
+public class enumDemo {
+
+    enum dayOfWeek {
+        Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday
+    }
+
+    enum foodItem {
+        apple, cheese, banana, orange, donut, ice_cream, hot_dog, waffle, pizza
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
